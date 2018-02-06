67a20810ab96345a6611bf2f1ebee0d6e2bff25b
Kristián Dienes <kristian.dienes@codexa.sk>
2018-02-06 23:07:07 +0100

    akka cluster

---
+++ src/main/scala/core/OrchestratorMessages.scala
@@ -0,0 +1,12 @@
+package core
+
+import akka.actor.ActorRef
+
+final case class Register(name: String, service: ActorRef)
+final case class FindByName(name: String)
+final case class FindAll()
+final case class StartService(id: String)
+final case class StopService(id: String)
+final case class GetState(id: String)
+
+case object OrchestratorMessages
